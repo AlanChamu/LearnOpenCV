cdbd61a1517ac986ba8db7c1e20922ae040f7b80
Alan Chamu <chamua@uci.edu>
2019-10-31 20:20:28 -0700

    merged lanedetection and rccortex

--- src/rccortex.py
+++ src/rccortex.py
@@ -1,5 +1,6 @@
 # yolov3 https://pjreddie.com/darknet/yolo/
 # source: https://towardsdatascience.com/object-detection-with-less-than-10-lines-of-code-using-python-2d28eebc5b11
+#source: https://www.youtube.com/watch?v=eLTLtUVuuy4
 
 # XXX: this code is too slow!
 # XXX: need to make faster
@@ -9,9 +10,9 @@ import cv2
 import numpy as np
 
 # to detect lanes
-import lanedetection
+# import lanedetection
 import matplotlib.pyplot as plt
-import tesla
+# import tesla
 
 # for the nn
 # import tesla
@@ -20,6 +21,137 @@ import tesla
 # import cvlib as cv
 # from cvlib.object_detection import draw_bbox
 # https://pjreddie.com/darknet/yolo/
+
+
+
+# XXX: TAKE BETTER test footage VIDEO, WITH TURNS
+
+# import numpy as np
+# import cv2
+# to get numerical values for region_of_interest()
+# import matplotlib.pyplot as plt
+
+def make_coordinates(image, line_parameters):
+    print("In make_coordinates,", image, line_parameters)
+
+    # result = []
+    try:
+        slope, intercept = line_parameters
+        y1 = image.shape[0]
+        print("y1,", y1)
+        # why do we have to convert to int?
+        y2 = int(y1*(3/5))
+        x1 = int((y1 - intercept)/slope)
+        x2 = int((y2 - intercept)/slope)
+        print("array,", [x1, y1, x2, y2])
+        return np.array([x1, y1, x2, y2])
+    except Exception as exc:
+        print("Oops,",exc)
+        # return None
+        # TEMPORARY FIX
+        return np.array([0, 0, 0, 0])
+    # finally:
+
+def average_slope_intercept(image, lines):
+    print("In average_slope_intercept ... ")
+    left_fit = []
+    right_fit = []
+
+    for line in lines:
+        x1, y1, x2, y2 = line.reshape(4)
+        parameters = np.polyfit((x1, x2), (y1, y2), 1)
+        slope = parameters[0]
+        intercept = parameters[1]
+
+        if slope < 0:
+            left_fit.append((slope, intercept))
+        else:
+            right_fit.append((slope, intercept))
+
+    # print("here?")
+    left_fit_average = np.average(left_fit, axis=0)
+    right_fit_average = np.average(right_fit, axis=0)
+    # print("here?")
+    left_line = make_coordinates(image, left_fit_average)
+    print("break 1")
+    right_line = make_coordinates(image, right_fit_average)
+    print("break 2")
+
+    # HAVE TO MAKE COMPATIBLE IF make_coordinates RETURNS A None
+    # BETTER YET, WHY IS THERE AN ISSUE IN make_coordinates?
+    return np.array([left_line, right_line])
+
+def canny(image):
+    #create a greyscale image
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY) #flag to turn gray
+    #GaussianBlur to REDUCE NOISE -> FOR MORE ACCURATE EDGE DETECTION
+    blur = cv2.GaussianBlur(gray, (5, 5), 0) # to reduce noise
+    # Step 3: detect lanes, figure out what Canny method does
+    # canny applies a 5by5 GaussianBlur either way
+    result = cv2.Canny(blur, 50, 150) # low to high threshold
+    return result
+
+# focuses HoughLinesP into a certain area in the mp4 file,
+# results in a smoother more accurrate line
+def region_of_interest(image):
+    height = image.shape[0]
+
+    polygons = np.array([
+    [(500, height), (1000, height), (800, 0)]
+    ])
+
+    # for test mp4 file from tutorial
+    # polygons = np.array([
+    # [(200, height), (1100, height), (550, 200)]
+    # ])
+
+    mask = np.zeros_like(image)
+    cv2.fillPoly(mask, polygons, 255)
+    masked_image = cv2.bitwise_and(image, mask)
+    return masked_image
+
+#now to make just the polygon display in front of the lanes
+
+# now display lines in front of lanes
+def display_lines(image, lines, color): # color is a three int tuple
+    line_image = np.zeros_like(image)
+    if lines is not None:
+        for x1, y1, x2, y2 in lines:
+            # last equals line thickness, 2nd to last is color BGR
+            cv2.line(line_image, (x1, y1), (x2, y2), color, 10)
+            # specify where you want it to be drawn, and color
+    return line_image
+
+def get_middle_line(lines):
+    print("in get_middle_line")
+
+    top = []
+    bottom = []
+
+    if lines is not None:
+        print(lines[0])
+        print(lines[1])
+
+        left = lines[0]
+        right = lines[1]
+
+        top_diff = (right[2] - left[2])/2
+        bottom_diff = (right[0] - left[0])/2
+
+        temp2 = int(top_diff) + left[2]
+        temp1 = temp2
+        # temp1 = int(bottom_diff) + left[0]  # will use this to get angle
+
+        print("top_diff=", top_diff)
+        print("bottom_diff=", bottom_diff)
+
+        top = np.array([temp1, 720, temp2, 432])
+        bottom = np.array([temp1, 720, temp2, 432])
+
+    return np.array([top,bottom])
+
+    #  have to return something like [[(x1, y1),(x2, y2)],
+    #                                   [(x1, y1), (x2, y2)]]
 
 # state will be the cars current state (up, stop, left, right)
 def handle_stop(state):
@@ -50,20 +182,20 @@ def detect_lane_from_video(video):
     while (cap.isOpened()):
         _, frame = cap.read()
         print("one")
-        cannyimg = lanedetection.canny(frame)
+        cannyimg = canny(frame)
         print("two")
-        cropped_image = lanedetection.region_of_interest(cannyimg)
+        cropped_image = region_of_interest(cannyimg)
         print("three")
         lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180,
             100, np.array([]), minLineLength=40, maxLineGap=5)
         print("four")
-        averaged_lines = lanedetection.average_slope_intercept(frame, lines)
+        averaged_lines = average_slope_intercept(frame, lines)
         print("five")
         # may have to return an angle value to allow car to turn directions
-        middle_line = lanedetection.get_middle_line(averaged_lines)
+        middle_line = get_middle_line(averaged_lines)
         print("six")
-        line_image = lanedetection.display_lines(frame, middle_line, (255, 0, 0))
-        # line_image = lanedetection.display_lines(frame, averaged_lines, (255, 0, 0))
+        line_image = display_lines(frame, middle_line, (255, 0, 0))
+        # line_image = display_lines(frame, averaged_lines, (255, 0, 0))
         print("seven")
         combo_img = cv2.addWeighted(frame, 0.8, line_image, 1, 1) # gamma value at end
         print("eight")
@@ -77,19 +209,3 @@ def detect_lane_from_video(video):
 
     cap.release()
     cv2.destroyAllWindows()
-
-def main(tesla):
-    print("Starting rcbellum.py ...")
-    video = "video1.mp4"
-    # video = "custom1.mp4"  # not good
-    # video = "custom2.mp4"
-    # video = "custom3.mp4"
-    try:
-        print(tesla)
-        detect_lane_from_video(video)
-    except Exception as exc:
-        print("Noooo,", exc)
-        # cap.release()
-        cv2.destroyAllWindows()
-    finally:
-        print("Done")
